811c3485b11349d4f28b0c8cdc5bca44f0ef6018
DaianCosta <desenvolvimentodaian@gmail.com>
2023-11-14 14:38:00 -0300

    test performance

--- src/scenarios/site/home.test.js
+++ src/scenarios/site/home.test.js
@@ -6,23 +6,28 @@ const iterationCountSuccess = new Counter('iteration_success')
 const iterationCountError = new Counter('iteration_error')
 
 export const options = {
-    vus: 1,
-    duration: '10s'
-}
+    stages: [
+        { duration: '10s', target: 5 },
+        { duration: '30s', target: 10 },
+        { duration: '60s', target: 100 },
+        { duration: '30s', target: 5 },
+        { duration: '10s', target: 0 },
+    ]
+};
 
-export default function(){
+export default function () {
     const BASE_URL = 'http://pizzafrontend.squad-test.svc.cluster.local:8080';
 
     const res = http.get(BASE_URL);
 
     if (!check(res, {
         'status code 200': (r) => r.status === 200
-    })){
+    })) {
         iterationCountError.add(1);
-        fail( `Fail consume. Returned ${res.status} status.`) 
-    }else{
+        fail(`Fail consume. Returned ${res.status} status.`)
+    } else {
         iterationCountSuccess.add(1)
     }
 
-    
+
 }
